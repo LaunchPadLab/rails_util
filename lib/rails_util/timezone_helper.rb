0fea3053e5c8adc0ecac429a01fb0eaafbbd3484
Ifat <iribon9@gmail.com>
2019-08-28 16:53:10 -0500

    +

--- lib/rails_util/timezone_helper.rb
+++ lib/rails_util/timezone_helper.rb
@@ -58,7 +58,8 @@ module RailsUtil
       def format_offset(timezone)
         offset = utc_offset(timezone).to_s
         offset.insert(1, '0') if utc_offset(timezone).abs < 10
-        offset + ':00'
+        plus_sign = '+' unless offset.include? '-'
+        [plus_sign, offset, ':00'].compact.join('')
       end
 
       # Returns a fixnum representation of the UTC offset for the given timezone in hours, taking into account whether Daylight Savings Time is in effect
